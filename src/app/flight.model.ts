978efe01f962792cb444ed3ca5251c0731cbac04
Nmuta <nmuta1@gmail.com>
2020-02-25 13:36:45 -0700

    1 file modified, 1 added

---
+++ src/app/flight.model.ts
@@ -0,0 +1,7 @@
+export interface flight
+{origin: string; 
+destination: string; 
+flightNumber: number; 
+depart: Date; 
+arrive: Date; 
+nonstop: boolean };
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FlightsService } from '../flights.service';
+import { flight } from '../flight';
 
 
 @Component({
